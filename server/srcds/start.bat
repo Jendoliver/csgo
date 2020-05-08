34d4da855a6ff6c61e6e5a1743fa625d7d37b29c
Jendoliver <alvarezmonllor@gmail.com>
2020-05-08 00:25:18 +0000

    Creates mg_brutal mapgroup, set by default Changes sv_password to exec sv_pass in autoexec for real

--- server/srcds/start.bat
+++ server/srcds/start.bat
@@ -2,5 +2,5 @@
 setlocal
 call ..\load_env.bat
 set /p id="Enter your SRCDS token (https://steamcommunity.com/dev/managegameservers): "
-start "" "%CSGO_SERVER_ROOT%\srcds" -game csgo -console -usercon +sv_setsteamaccount %id% -net_port_try 1 +game_type 0 +game_mode 0 +mapgroup mg_active +map de_dust2
+start "" "%CSGO_SERVER_ROOT%\srcds" -game csgo -console -usercon +sv_setsteamaccount %id% -net_port_try 1 +game_type 0 +game_mode 0 +mapgroup mg_brutal +map am_tocador
 endlocal
